7ed96e7a649cc96781cc2cda06bd7db4877828b7
sam gaudet <samgaudet@sams-Mac-mini.telus>
2020-06-07 10:28:06 -0600

    added 4 digit 7 segment display to show how many people can go in

--- Distance_counter/Distance_counter.ino
+++ Distance_counter/Distance_counter.ino
@@ -5,6 +5,20 @@ const int outEcho = 7;
 const int redPin = 10;
 const int greenPin = 11;
 
+//7 seg pins
+const int a = 2;
+const int b = 3;
+const int c = 4;
+const int d = 5;
+const int e = 10;
+const int f = 11;
+const int g = 12;
+
+const int D1 = A0;
+const int D2 = A1;
+const int D3 = A2;
+const int D4 = A3;
+
 const int maxInactiveDist = 20;
 
 bool inTriggered = false;
@@ -17,7 +31,7 @@ unsigned long inTriggeredAt = 99999999999;
 unsigned long outTriggeredAt = 99999999999;
 
 int people = 0;
-int maxPeople = 3;
+int maxPeople = 11;
 
 bool atCapacity = false;
 
@@ -29,20 +43,95 @@ void setup()
   pinMode(outPing, OUTPUT);
   pinMode(outEcho, INPUT);
   pinMode(redPin, OUTPUT);
+
+  pinMode(a, OUTPUT);     
+  pinMode(b, OUTPUT);     
+  pinMode(c, OUTPUT);     
+  pinMode(d, OUTPUT);     
+  pinMode(e, OUTPUT);     
+  pinMode(f, OUTPUT);     
+  pinMode(g, OUTPUT); 
+    
+  pinMode(D1, OUTPUT);  
+  pinMode(D2, OUTPUT);  
+  pinMode(D3, OUTPUT);  
+  pinMode(D4, OUTPUT); 
 }
 
 void loop()
-{
+{ 
   checkPeople();
   
-  updateLights();
+//  updateLights();
+  getPeopleThatCanEnter();
+
+//  Serial.print("people: ");
+//  Serial.print(people);
+//  Serial.println();
   
-  Serial.print("people: ");
-  Serial.print(people);
-  Serial.println();
-  
-  delay(100);
+//  delay(100);
 }
+
+void getPeopleThatCanEnter() {
+  int peopleThatCanEnter = maxPeople - people;\
+  if(peopleThatCanEnter <= 0) {
+    peopleThatCanEnter = 0;
+  }
+  int ones = peopleThatCanEnter % 10;
+  int tens = (peopleThatCanEnter / 10) % 10;
+  set7Seg(ones, 1);
+  delay(10);
+  set7Seg(tens, 2);
+}
+
+
+/*
+    A
+   ---
+F |   | B
+  | G |
+   ---
+E |   | C
+  |   |
+   ---
+    D
+ */
+int numbers[][7] {
+//{ a, b, c, d, e, f, g }
+  { LOW, LOW, LOW, LOW, LOW, LOW, HIGH }, //0
+  { HIGH, LOW, LOW, HIGH, HIGH, HIGH, HIGH }, // 1
+  { LOW, LOW, HIGH, LOW, LOW, HIGH, LOW },// 2
+  { LOW, LOW, LOW, LOW, HIGH, HIGH, LOW },//3
+  { HIGH, LOW, LOW, HIGH, HIGH, LOW, LOW },//4
+  { LOW, HIGH, LOW, LOW, HIGH, LOW, LOW },//5
+  { LOW, HIGH, LOW, LOW, LOW, LOW, LOW },//6
+  { LOW, LOW, LOW, HIGH, HIGH, HIGH, HIGH },//7
+  { LOW, LOW, LOW, LOW, LOW, LOW, LOW },//8
+  { LOW, LOW, LOW, LOW, HIGH, LOW, LOW },//9
+};
+
+void set7Seg(int num, int digit) {
+  if(digit == 2) {
+    digitalWrite(D1, LOW);
+    digitalWrite(D2, LOW);
+    digitalWrite(D3, HIGH);
+    digitalWrite(D4, LOW);
+  } else {
+    digitalWrite(D1, LOW);
+    digitalWrite(D2, LOW);
+    digitalWrite(D3, LOW);
+    digitalWrite(D4, HIGH);
+  }
+
+  digitalWrite(a, numbers[num][0]);
+  digitalWrite(b, numbers[num][1]);
+  digitalWrite(c, numbers[num][2]);
+  digitalWrite(d, numbers[num][3]);
+  digitalWrite(e, numbers[num][4]);
+  digitalWrite(f, numbers[num][5]);
+  digitalWrite(g, numbers[num][6]);
+}
+
 
 void updatePeople(int incrament) {
   people += incrament;
@@ -51,11 +140,11 @@ void updatePeople(int incrament) {
   }
   if(people >= maxPeople) {
     atCapacity = true;
+    Serial.println();
     Serial.print("at capacity");
   } else {
     atCapacity = false;
   }
-  
 }
 
 long microsecondsToCentimeters(long microseconds) {
@@ -106,13 +195,6 @@ void checkPeople() {
     inTriggeredAt = 99999999999;
     outTriggeredAt = 99999999999;
   }
-//  Serial.print("in: ");
-//  Serial.print(inDist);
-//  Serial.println();
-//  Serial.print("out: ");
-//  Serial.print(outDist);
-//  Serial.println();
-//  
 }
 
 void getDist() {
